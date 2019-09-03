d9bd50edd492385fec84a07e0dbdf43bde9613cc
vedanta-krit.das <vedmalex@gmail.com>
2019-09-03 17:10:03 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Алексей Ведмеденко on 03/09/2019.
+//  Copyright © 2019 Алексей Ведмеденко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
